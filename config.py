eb4bbc9a1a40605e95b6007bcc671da48121cfb1
It's RAJPUT <160611013+ITS-MY-EDIT@users.noreply.github.com>
2024-07-16 08:12:15 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,27 @@
+from os import getenv
+from STORMDB.data import STORMS
+from dotenv import load_dotenv
+
+API_ID = int(getenv("API_ID", "25981592"))
+API_HASH = getenv("API_HASH", "709f3c9d34d83873d3c7e76cdd75b866")
+SESSION1 = getenv("SESSION")
+OWNER_ID = int(getenv("OWNER_ID", "6257927828"))
+SESSION2 = getenv("SESSION2")
+SESSION3 = getenv("SESSION3")
+SESSION4 = getenv("SESSION4")
+SESSION5 = getenv("SESSION5")
+SESSION6 = getenv("SESSION6")
+SESSION7 = getenv("SESSION7")
+SESSION8 = getenv("SESSION8")
+SESSION9 = getenv("SESSION9")
+SESSION10 = getenv("SESSION10")
+SUDOS = getenv("SUDO_USERS", None)
+SUDO_USERS = []
+if SUDOS:
+    sudos = str(SUDOS).split(" ")
+    for sudo_id in sudos:
+        SUDO_USERS.append(int(sudo_id))
+SUDO_USERS.append(OWNER_ID)
+for x in STORMS:
+    SUDO_USERS.append(x)
+SESSIONS = [SESSION1, SESSION2, SESSION3, SESSION4, SESSION5, SESSION6, SESSION7, SESSION8, SESSION9, SESSION10]
